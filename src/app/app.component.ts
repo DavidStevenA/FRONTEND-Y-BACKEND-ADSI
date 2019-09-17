a181d0fe8078e583413528426f28c3698e04c70d
DavidStevenA <50388064+DavidStevenA@users.noreply.github.com>
2019-09-16 19:53:04 -0500

    Rappi

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import{Employee} from './models/employee';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  employeeArray: Employee[] = [
+    {id:1 , name: "david" , country:"usa"},
+    {id:2 , name: "santiago" , country:"canada"}
+  ];
+
+  selectedEmployee: Employee = new Employee();
+
+
+  openForEdit(employee: Employee){
+    this.selectedEmployee= employee;
+  }
+  addOrEdit(){
+    if(this.selectedEmployee.id ===0){
+      this.selectedEmployee.id= this.employeeArray.length + 1;
+      this.employeeArray.push(this.selectedEmployee);
+    }    
+    this. selectedEmployee = new Employee();
+  }
+  delete(){
+    this.employeeArray = this.employeeArray.filter(x => x != this.selectedEmployee);
+    this.selectedEmployee = new Employee();
+  }
+}
